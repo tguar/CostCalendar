94ecbd39927cf9ec6aa22e858de3bcba1a462698
victorh3 <victorh3@live.com>
2019-08-04 09:46:55 -0500

    Create simple layout structure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="container">
       <h1 className="Header">Cost Calculator</h1>
+      <div className="row">
+        <div className="col-sm-8">
+          <h2 className="Calendar">Cost in Days of Work</h2>
+        </div>
+        <div className="col-sm-4">
+          <h2 className="Expenses">Expenses</h2>
+        </div>
+      </div>
     </div>
   );
 }
