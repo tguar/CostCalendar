aa5266ef36573931a0dc9e31f4cf40796e577b48
Tom G <thomas.guarnery@gmail.com>
2019-08-06 17:57:04 -0700

    chore: change double quote to single

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { StoreProvider } from "./Store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { StoreProvider } from './Store';
 
 ReactDOM.render(
   <StoreProvider>
