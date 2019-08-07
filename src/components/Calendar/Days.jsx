25e9ee07de14b01d9311a1f32b1e0875aa32785c
Tom G <thomas.guarnery@gmail.com>
2019-08-06 17:56:42 -0700

    chore: change double quote to single quote

--- src/components/Calendar/Days.jsx
+++ src/components/Calendar/Days.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import styled from "styled-components";
-import Day from "./Day";
+import React from 'react';
+import styled from 'styled-components';
+import Day from './Day';
 
 const DaysWrapper = styled.div`
   display: flex;
--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,4 +1,4 @@
-export { default } from "./Calendar";
-export { default as Month } from "./Month";
-export { default as Days } from "./Days";
-export { default as Day } from "./Day";
+export { default } from './Calendar';
+export { default as Month } from './Month';
+export { default as Days } from './Days';
+export { default as Day } from './Day';
