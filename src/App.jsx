28aa2b4239e8df24a2fbd74fa69d88bcabc82a8c
Tom G <thomas.guarnery@gmail.com>
2019-08-11 13:09:35 -0700

    fix: update calendar import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Calendar from './Components/Calendar';
+import Calendar from './Components/Calendar/Calendar';
 import List from './Components/List';
 
 function App() {
