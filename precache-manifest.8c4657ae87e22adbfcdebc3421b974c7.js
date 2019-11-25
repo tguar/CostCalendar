2bbbfc71fd6c3e54b99a66735dbd1e8174ee0337
victorh3 <victorh3@live.com>
2019-11-24 21:13:15 -0600

    Updates

--- precache-manifest.8c4657ae87e22adbfcdebc3421b974c7.js
+++ precache-manifest.8c4657ae87e22adbfcdebc3421b974c7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f67c31ed4472a20ad7ad767bad988d44",
+    "revision": "0b3e2de5b56668cbdf3911f51653c78a",
     "url": "/CostCalendar/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/CostCalendar/static/css/2.0bf6fa00.chunk.css"
   },
   {
-    "revision": "20f7bbe4d4b0cb715b12",
+    "revision": "205a83fae78492dfe145",
     "url": "/CostCalendar/static/css/main.7544d863.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/CostCalendar/static/js/2.6a7aaea4.chunk.js"
   },
   {
-    "revision": "20f7bbe4d4b0cb715b12",
-    "url": "/CostCalendar/static/js/main.0bdec6e5.chunk.js"
+    "revision": "205a83fae78492dfe145",
+    "url": "/CostCalendar/static/js/main.d0849653.chunk.js"
   },
   {
     "revision": "2eecd264e65bef0b6434",
