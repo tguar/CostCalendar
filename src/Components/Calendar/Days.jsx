544f47cec5c88541240c4072ad0eedf6c65e768d
Tom G <thomas.guarnery@gmail.com>
2020-02-06 19:58:19 -0700

    removed unused import

--- src/Components/Calendar/Days.jsx
+++ src/Components/Calendar/Days.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Day from './Day';
 import { removeCommasFromString } from '../../helpers';
